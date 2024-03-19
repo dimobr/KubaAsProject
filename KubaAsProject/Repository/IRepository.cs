d8ed6f8465d0e45f9355cd3b551acf33e4acc89a
dimobr <d96obradovic@gmail.com>
2024-03-19 16:35:35 +0100

    Add generic repository interface and its implementation.

---
+++ KubaAsProject/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using KubaAsProject.Models;
+
+namespace KubaAsProject.Repository
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+        Task DeleteByIdAsync(int id);
+    }
+}
---
+++ KubaAsProject/Repository/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using KubaAsProject.AppDatabaseContext;
+using KubaAsProject.Models;
+
+namespace KubaAsProject.Repository
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+        private readonly AppDbContext _dbContext;
+
+        public Repository(AppDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<TEntity>().FirstOrDefaultAsync(entity => entity.Id == id) 
+                ?? throw new NullReferenceException($"Object with Id = {id} could not be found or does not exist.");
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<TEntity> DeleteAsync(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity is not null)
+            {
+                _dbContext.Set<TEntity>().Remove(entity);
+            }
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
