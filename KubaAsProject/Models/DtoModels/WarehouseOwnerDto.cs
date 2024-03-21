3989ce2355d3a159cc698fea08e16fa67feca92d
dimobr <d96obradovic@gmail.com>
2024-03-21 23:21:39 +0100

    Add DTO models.

---
+++ KubaAsProject/Models/DtoModels/WarehouseOwnerDto.cs
@@ -0,0 +1,16 @@
+﻿namespace KubaAsProject.Models.DtoModels
+{
+    public class WarehouseOwnerDto
+    {
+        public string Name { get; set; }
+        public bool? IsBusiness { get; set; }
+        public string? ContactPerson { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Street { get; set; }
+        public string? PostalCode { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? Country { get; set; }
+    }
+}
