8d667d7272b016a5e75549407c8ff44b1f35b0d8
dimobr <d96obradovic@gmail.com>
2024-03-25 22:22:44 +0100

    Add DTO models for addresses and owners with warehouses.

---
+++ KubaAsProject/Models/DtoModels/AddressDto.cs
@@ -0,0 +1,11 @@
+﻿namespace KubaAsProject.Models.DtoModels
+{
+    public class AddressDto
+    {
+        public string? Street { get; set; }
+        public string? PostalCode { get; set; }
+        public string? City { get; set; }
+        public string? Region { get; set; }
+        public string? Country { get; set; }
+    }
+}
---
+++ KubaAsProject/Models/DtoModels/OwnerAndWarehousesDto.cs
@@ -0,0 +1,13 @@
+﻿namespace KubaAsProject.Models.DtoModels
+{
+    public class OwnerAndWarehousesDto
+    {
+        public string Name { get; set; }
+        public bool? IsBusiness { get; set; }
+        public string? ContactPerson { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public AddressDto Address { get; set; } 
+        public List<WarehouseDto> Warehouses { get; set; }
+    }
+}
