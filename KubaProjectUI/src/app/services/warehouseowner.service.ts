941fc48e2b86368310f20629c11c71a646498880
dimobr <d96obradovic@gmail.com>
2024-03-25 22:19:33 +0100

    Add homepage route in UI.

--- KubaProjectUI/src/app/services/warehouseowner.service.ts
+++ KubaProjectUI/src/app/services/warehouseowner.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WarehouseownerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getOwnerById(id: number): Observable<any> {
+    let params = new HttpParams().set('id', id.toString());
+    return this.http.get<any>('api/getOwner', { params });
+  }
 }
